8e3a4f403102a9a67d730302c18f882aaa9164d0
Vojta Holik <vojta@egghead.io>
2020-07-14 16:01:54 +0200

    hmm

--- functions/gen-image/gen-image.js
+++ functions/gen-image/gen-image.js
@@ -1,7 +1,6 @@
 const playwright = require("playwright-aws-lambda")
 const fs = require("fs")
 const script = fs.readFileSync("./src/talk.js", "utf-8")
-const generateCourseImage = require("./gen-course")
 
 exports.handler = async function (event, ctx) {
   const browser = await playwright.launchChromium()
@@ -10,18 +9,15 @@ exports.handler = async function (event, ctx) {
 
   const {queryStringParameters} = event
 
-  if (queryStringParameters.type === "course") {
-    await generateCourseImage(browser, page, queryStringParameters)
-  } else {
-    page.setViewportSize({
-      // video poster
-      width: 1280,
-      height: 720,
-      // og image
-      // width: 1200,
-      // height: 630,
-    })
-    await page.setContent(`<!DOCTYPE html>
+  page.setViewportSize({
+    // video poster
+    width: 1280,
+    height: 720,
+    // og image
+    // width: 1200,
+    // height: 630,
+  })
+  await page.setContent(`<!DOCTYPE html>
   <html>
     <head>
       <meta charset="utf-8" />
@@ -32,42 +28,41 @@ exports.handler = async function (event, ctx) {
     </body>
   </html>
   `)
-    const tags = queryStringParameters.tags
-      ? decodeURIComponent(queryStringParameters.tags).split(",")
-      : []
-    await page.addScriptTag({
-      content: `
+  const tags = queryStringParameters.tags
+    ? decodeURIComponent(queryStringParameters.tags).split(",")
+    : []
+  await page.addScriptTag({
+    content: `
   window.title = "${queryStringParameters.title || "No Title"}";
   window.tags = ${JSON.stringify(tags)};
   window.author = "${queryStringParameters.author || ""}";
   window.image = "https://joelhooks.com/images/logo.png";
   window.slug = "${queryStringParameters.slug || "No Slug"}";
   `,
-    })
-    await page.addScriptTag({content: script})
-    const boundingRect = await page.evaluate(() => {
-      const corgi = document.getElementById("corgi")
-      const {x, y, width, height} = corgi.children[0].getBoundingClientRect()
+  })
+  await page.addScriptTag({content: script})
+  const boundingRect = await page.evaluate(() => {
+    const corgi = document.getElementById("corgi")
+    const {x, y, width, height} = corgi.children[0].getBoundingClientRect()
 
-      return {x, y, width, height}
-    })
+    return {x, y, width, height}
+  })
 
-    try {
-      await page.waitForResponse(/twemoji.maxcdn.com/, {timeout: 1200})
-    } catch (e) {
-      console.log(e)
-    }
+  try {
+    await page.waitForResponse(/twemoji.maxcdn.com/, {timeout: 1200})
+  } catch (e) {
+    console.log(e)
+  }
 
-    const screenshotBuffer = await page.screenshot({clip: boundingRect})
-    await browser.close()
-    return {
-      isBase64Encoded: true,
-      statusCode: 200,
-      headers: {
-        "Content-Type": "image/png",
-        "Content-Length": screenshotBuffer.length.toString(),
-      },
-      body: screenshotBuffer.toString("base64"),
-    }
+  const screenshotBuffer = await page.screenshot({clip: boundingRect})
+  await browser.close()
+  return {
+    isBase64Encoded: true,
+    statusCode: 200,
+    headers: {
+      "Content-Type": "image/png",
+      "Content-Length": screenshotBuffer.length.toString(),
+    },
+    body: screenshotBuffer.toString("base64"),
   }
 }
